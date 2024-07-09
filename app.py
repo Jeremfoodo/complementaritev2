285405fcfe9aa8fd4e8f16534bef28e412beb448
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-09 09:53:12 +0200

    Update app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+from pages.main_page import main_page
+from pages.segmentation_page import segmentation_page
+
+# Créer le menu de navigation
+selected_page = option_menu(
+    menu_title=None,
+    options=["Analyse", "Segmentation"],
+    icons=["bar-chart-line", "pie-chart"],
+    menu_icon="cast",
+    default_index=0,
+    orientation="horizontal",
+)
+
+# Afficher la page sélectionnée
+if selected_page == "Analyse":
+    main_page()
+elif selected_page == "Segmentation":
+    segmentation_page()
