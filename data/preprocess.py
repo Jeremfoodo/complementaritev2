be8121212604617b7e0439720d750ec021718e6d
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-09 11:36:29 +0200

    Update preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,17 +1,25 @@
 import pandas as pd
-import streamlit as st
 
-
-@st.cache_data
 def load_and_preprocess(file_path):
-    data = pd.read_excel(file_path, sheet_name='Export')
-    data = data[data[data.columns[0]] != "Total"]
-    data = data.dropna(subset=[data.columns[0]])
-    data = data[~data[data.columns[0]].astype(str).str.startswith("Filtres")]
-    data = data.rename(columns={'GMV': 'GMV WITH TAX'})
-    data['Date'] = pd.to_datetime(data['Date'], errors='coerce')
-    return data
+    try:
+        data = pd.read_excel(file_path, sheet_name='Export')
+        print("Données chargées avec succès")
+        data = data[data[data.columns[0]] != "Total"]
+        data = data.dropna(subset=[data.columns[0]])
+        data = data[~data[data.columns[0]].astype(str).str.startswith("Filtres")]
+        data = data.rename(columns={'GMV': 'GMV WITH TAX'})
+        data['Date'] = pd.to_datetime(data['Date'], errors='coerce')
+        print("Données prétraitées avec succès")
+        return data
+    except Exception as e:
+        print(f"Erreur lors du chargement et du prétraitement des données : {e}")
+        return pd.DataFrame()
 
-@st.cache_data
 def process_data(data):
-    return data.groupby('order_id')['product_name'].apply(list).tolist()
+    try:
+        transactions = data.groupby('order_id')['product_name'].apply(list).tolist()
+        print("Données traitées avec succès")
+        return transactions
+    except Exception as e:
+        print(f"Erreur lors du traitement des données : {e}")
+        return []
