7ef3c12b1062d28c9b037b2a819f03e4056f82db
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-09 10:51:32 +0200

    Update download.py

--- data/download.py
+++ data/download.py
@@ -1,14 +1,12 @@
 import gdown
 import pandas as pd
 import streamlit as st
-from data.preprocess import load_and_preprocess
 
-@st.cache
+# Désactiver le cache temporairement
 def download_data(url, output_path):
     gdown.download(url, output_path, quiet=True)
     return load_and_preprocess(output_path)
 
-@st.cache
 def download_segmentation_data(url, output_path):
     gdown.download(url, output_path, quiet=True)
     return pd.read_excel(output_path)
