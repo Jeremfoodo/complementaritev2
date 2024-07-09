a63b49bb10a504c215cf4aefc9018cc920c81596
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-09 11:10:57 +0200

    Update main_page.py

--- pages/main_page.py
+++ pages/main_page.py
@@ -27,6 +27,8 @@ def main_page():
     output_path = output_paths[user_country]
     data = download_data(file_url, output_path)
 
+    st.write("Données chargées :", data.head())
+
     if data.empty:
         st.error("Les données n'ont pas été correctement chargées pour ce pays.")
     else:
@@ -43,6 +45,8 @@ def main_page():
         else:
             user_region = user_country
             data_region_selected = data
+
+        st.write("Données pour la région sélectionnée :", data_region_selected.head())
 
         if data_region_selected.empty:
             st.error("Aucune donnée disponible pour cette région.")
@@ -66,29 +70,4 @@ def main_page():
                     rules_single = rules[(rules['antecedents'].apply(len) == 1) & (rules['consequents'].apply(len) == 1)]
                     rules_single['antecedents'] = rules_single['antecedents'].apply(lambda x: ', '.join(x))
                     rules_single['consequents'] = rules_single['consequents'].apply(lambda x: ', '.join(x))
-                    rules_single[['antecedent support', 'consequent support', 'support', 'confidence', 'lift', 'leverage', 'conviction']] = \
-                        rules_single[['antecedent support', 'consequent support', 'support', 'confidence', 'lift', 'leverage', 'conviction']].round(2)
-                    rules_high_support = rules_single[rules_single['antecedent support'] > 0.1]
-                    rules_chosen_product = rules_single[rules_single['antecedents'] == chosen_product]
-                    columns_to_display = ['antecedents', 'consequents', 'antecedent support', 'consequent support',
-                                          'support', 'confidence', 'lift', 'leverage', 'conviction']
-                    rules_display = rules_chosen_product[columns_to_display]
-                    rules_display.columns = ['Antécédents', 'Conséquents', 'Support Antécédent', 'Support Conséquent',
-                                             'Support', 'Confiance', 'Lift', 'Leverage', 'Conviction']
-                    rules_display.sort_values(by=['Leverage', 'Support Antécédent', 'Confiance', 'Lift'], ascending=[False, False, False, False], inplace=True)
-                    st.subheader("Résultats de l'Analyse")
-                    st.markdown(
-                        """
-                        <div style="background-color: #f0f0f0; padding: 10px; border-radius: 5px; font-size: 0.5em;">
-                            <p><strong>Pour rappel, important pour vous :</strong></p>
-                            <p><strong>Support Antécédent</strong> : le % de commande qui contiennent le produit "Antécédent"</p>
-                            <p><strong>Support Conséquent</strong> : le % de commande qui contiennent le produit "Conséquent"</p>
-                            <p><strong>Support</strong> : le % de commande qui contiennent au moins ces 2 articles ensemble</p>
-                            <p><strong>Lift </strong> : Nombre de chance supérieure qu'à le produit Conséquent a être acheté avec le produit Antécédent plutôt que tout seul. C'est l'indicateur principal de corrélation.</p>
-                            <p>Vous pouvez trier le tableau en cliquant sur les colonnes, et aussi télécharger la liste (passer la souris sur le tableau et download as csv)</p>
-                        </div>
-                        """,
-                        unsafe_allow_html=True
-                    )
-                    st.markdown("<br>", unsafe_allow_html=True)
-                    st.dataframe(rules_display.head(50))
+                    rules
