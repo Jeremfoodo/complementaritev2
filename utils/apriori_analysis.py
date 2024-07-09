f8071dbd81a98c1099b94f450aa5da05d0e2342b
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-09 09:54:41 +0200

    Update apriori_analysis.py

---
+++ utils/apriori_analysis.py
@@ -0,0 +1,12 @@
+from mlxtend.preprocessing import TransactionEncoder
+from mlxtend.frequent_patterns import apriori, association_rules
+import pandas as pd
+
+@st.cache_data
+def apriori_rules(transactions, min_support=0.01, min_lift=1.2):
+    encoder = TransactionEncoder()
+    onehot = encoder.fit_transform(transactions)
+    df_onehot = pd.DataFrame(onehot, columns=encoder.columns_)
+    frequent_itemsets = apriori(df_onehot, min_support=min_support, use_colnames=True)
+    rules = association_rules(frequent_itemsets, metric="lift", min_threshold=min_lift)
+    return rules
