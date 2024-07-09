e392c7c0bad6f084f3f1da52ec0b8c60ff27ad8a
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-07-09 10:02:05 +0200

    Update segmentation_page.py

--- pages/segmentation_page.py
+++ pages/segmentation_page.py
@@ -6,15 +6,20 @@ import matplotlib.pyplot as plt
 
 def segmentation_page():
     st.title("Segmentation des Clients")
+    
     segmentation_url = 'https://docs.google.com/spreadsheets/d/1lkxXgC095L0OQdItsGYtisZqVk_nXkPa/export?format=xlsx'
     segmentation_output_path = 'segmentationFR.xlsx'
     segmentation_data = download_segmentation_data(segmentation_url, segmentation_output_path)
+
     zones = ['Toute France', 'Paris', 'Paris EST', 'Paris Ouest', 'Province']
     selected_zone = st.selectbox("Choisissez la zone :", options=zones)
+
     categories = ['Fruits et Légumes', 'Boucherie', 'Epicerie salée', 'Crémerie', 'Toutes catégories']
     selected_category = st.selectbox("Choisissez la catégorie :", options=categories)
+
     months = ['avril', 'mai', 'juin', '3 last month']
     selected_month = st.selectbox("Choisissez le mois :", options=months)
+
     if st.button("Lancer l'analyse"):
         file_urls = {
             'France': 'https://docs.google.com/spreadsheets/d/1_qfuH19aLu3JMocit2-wSL-FCmWNet1I/export?format=xlsx'
@@ -22,14 +27,19 @@ def segmentation_page():
         output_paths = {
             'France': 'dataFR.xlsx'
         }
+
         france_data = download_data(file_urls['France'], output_paths['France'])
         france_data['Date'] = pd.to_datetime(france_data['Date'], errors='coerce')
+
         st.write("Colonnes dans france_data:", france_data.columns)
         st.write("Colonnes dans segmentation_data:", segmentation_data.columns)
+
         if selected_zone != 'Toute France':
             france_data = france_data[france_data['region'] == selected_zone]
+
         if selected_category != 'Toutes catégories':
             france_data = france_data[france_data['Product Category'] == selected_category]
+
         if selected_month == 'avril':
             france_data = france_data[france_data['Date'].dt.month == 4]
         elif selected_month == 'mai':
@@ -38,8 +48,12 @@ def segmentation_page():
             france_data = france_data[france_data['Date'].dt.month == 6]
         elif selected_month == '3 last month':
             france_data = france_data[france_data['Date'].dt.month.isin([4, 5, 6])]
-        merged_data = france_data.merge(segmentation_data, left_on='client_id', right_on='Restaurant_id')
+
+        # Corrigez les colonnes pour la fusion
+        merged_data = france_data.merge(segmentation_data, on='Restaurant_id')
+
         segment_counts = merged_data.groupby(['Gamme', 'Type']).size().unstack(fill_value=0)
+
         plt.figure(figsize=(10, 6))
         sns.heatmap(segment_counts, annot=True, fmt="d", cmap="YlGnBu")
         plt.title("Heatmap du nombre de clients pour chaque segment")
