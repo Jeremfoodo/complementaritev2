78d2fc466ff10585cf6b802270daf3d6e9c7ddbc
Jeremfoodo <jeremy.stoss@foodomarket.com>
2024-11-28 11:54:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,17 @@ from streamlit_option_menu import option_menu
 from pages.main_page import main_page
 from pages.segmentation_page import segmentation_page
 
-# Créer le menu de navigation
+# Configuration de la page principale
+st.set_page_config(
+    page_title="Analyse de Produits et Segmentation",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
+
+# Menu de navigation
 selected_page = option_menu(
     menu_title=None,
-    options=["Analyse", "Segmentation"],
+    options=["Analyse de complémentarité", "Segmentation"],
     icons=["bar-chart-line", "pie-chart"],
     menu_icon="cast",
     default_index=0,
@@ -14,7 +21,7 @@ selected_page = option_menu(
 )
 
 # Afficher la page sélectionnée
-if selected_page == "Analyse":
+if selected_page == "Analyse de complémentarité":
     main_page()
 elif selected_page == "Segmentation":
     segmentation_page()
